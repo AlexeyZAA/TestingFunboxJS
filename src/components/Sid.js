e8884a6934ede781e5f35413b827aeedc5a95af7
zaa <zaa@tk-barn.ru>
2020-01-22 10:09:41 +0700

    Изменен компонент списка

--- src/components/Sid.js
+++ src/components/Sid.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { sortableContainer, sortableElement } from "react-sortable-hoc";
+import arrayMove from "array-move";
+import { Layout, Input, List, Button } from "antd";
 
-import { Layout, Input, List, Button } from 'antd';
+const { Sider } = Layout;
 
+const SortableItem = sortableElement(({ value }) => <li>{value}</li>);
 
-
-const { Sider } = Layout
+const SortableContainer = sortableContainer(({ children }) => {
+  return <ul>{children}</ul>;
+});
 
 class Sid extends React.Component {
   state = {
     collapsed: false,
     inputpoint: "",
-    datapointlist: []
+    datapointlist: [],
+    items: ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"],
+  };
+
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    this.setState(({ items }) => ({
+      items: arrayMove(items, oldIndex, newIndex),
+    }));
   };
 
   toggle = () => {
@@ -20,41 +32,43 @@ class Sid extends React.Component {
   };
 
   handleEnter = event => {
-  if (event.keyCode === 13) {
-    
+    if (event.keyCode === 13) {
+      this.setState({
+        inputpoint: event.target.value,
+      });
+      this.state.datapointlist.push({
+        title: event.target.value,
+        key: this.state.datapointlist.length,
+      });
+    }
+  };
+  inputPointClean = event => {
+    if (event.keyCode === 13) event.target.value = "";
+  };
+
+  itempointDel = param => {
+    let newitemarr = [];
+    newitemarr = this.state.datapointlist.filter(item => {
+      return item.key !== param.key;
+    });
+
+    for (let i = 0; i < newitemarr.length; i++) {
+      newitemarr[i]["key"] = i;
+    }
     this.setState({
-      inputpoint: event.target.value
-    })
-    this.state.datapointlist.push({title: event.target.value, key: this.state.datapointlist.length})  
-  }
-};
-inputPointClean = event => {
-  if (event.keyCode === 13) event.target.value = ""
-};
+      datapointlist: newitemarr,
+    });
 
-itempointDel = (param) => {
-  let newitemarr = []
-  newitemarr = this.state.datapointlist.filter( (item) => {
-     return item.key !== param.key 
-  })
-  
-for (let i=0; i < newitemarr.length; i++){
-  newitemarr[i]['key'] = i 
-}
-  this.setState({
-   datapointlist: newitemarr
- })
-
- this.setState({
-  inputpoint: ""
-  })
- };
-
+    this.setState({
+      inputpoint: "",
+    });
+  };
 
   render() {
-      return (
+    const { items } = this.state;
 
-        <Sider
+    return (
+      <Sider
         breakpoint="lg"
         collapsedWidth="0"
         theme="light"
@@ -65,27 +79,20 @@ for (let i=0; i < newitemarr.length; i++){
           console.log(collapsed, type);
         }}
       >
-      <Input placeholder="Новая точка маршрута" onKeyUp={ this.inputPointClean } onKeyDown={ this.handleEnter } />
-      <div class="listpoint">
-
-      <List
-          itemLayout="horizontal"
-          dataSource={this.state.datapointlist}
-          renderItem={item => (
-            <List.Item key={item.key}>
-              <List.Item.Meta
-                title={item.title}
-              />
-            <div>
-            <Button type="primary" shape="circle" onClick={() => this.itempointDel({key: item.key, titl: item.title})} >del</Button>
-            </div>
-            </List.Item>
-          )}
+        <Input
+          placeholder="Новая точка маршрута"
+          onKeyUp={this.inputPointClean}
+          onKeyDown={this.handleEnter}
         />
-       </div> 
-      </Sider>
-      );
-    }
-  }
 
-  export default Sid;
+        <SortableContainer onSortEnd={this.onSortEnd}>
+          {items.map((value, index) => (
+            <SortableItem key={`item-${value}`} index={index} value={value} />
+          ))}
+        </SortableContainer>
+      </Sider>
+    );
+  }
+}
+
+export default Sid;
--- src/components/ym/Ymap.js
+++ src/components/ym/Ymap.js
@@ -6,14 +6,10 @@ const mapState = { center: [53.353929, 83.768455], zoom: 15 };
 
 let geoArr = [<Placemark geometry={{ type: 'Circle', coordinates: [53.353741, 83.768138], radius: 15 }} />, <Placemark geometry={{ type: 'Circle', coordinates: [53.353741, 83.768138], radius: 15 }} />];
 
-
-
 const Ymap = () => (
   <YMaps>
     <Map state={mapState} width="700px" height="500px">
-    {geoArr.map((geo, i) =>
-        <Placemark key={i} {...geo} />
-      )}
+
     </Map>
   </YMaps>
 );
