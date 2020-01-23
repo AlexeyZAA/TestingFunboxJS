3725ed227d36be2bc6032c7b37fbc305dbb934f7
AlexeyZAA <nibbler2012@mail.ru>
2020-01-23 20:53:54 +0600

    fix react down

--- src/components/ym/Ymap.js
+++ src/components/ym/Ymap.js
@@ -1,17 +1,40 @@
-import React, { Component } from "react";
-
+import React from "react";
 import { YMaps, Map, GeoObject, Placemark } from "react-yandex-maps";
 
 const mapState = { center: [53.353929, 83.768455], zoom: 15 };
 
-let geoArr = [<Placemark geometry={{ type: 'Circle', coordinates: [53.353741, 83.768138], radius: 15 }} />, <Placemark geometry={{ type: 'Circle', coordinates: [53.353741, 83.768138], radius: 15 }} />];
+class Ymap extends React.Component {
+  state = {
+    center: mapState.center
+  };
 
-const Ymap = () => (
-  <YMaps>
-    <Map state={mapState} width="700px" height="500px">
+  onBoundsChange = () => {
+    this.setState({
+      center: this.state.map.getCenter()
+    });
+  };
 
-    </Map>
-  </YMaps>
-);
+  instPoint = () => {
+    alert(JSON.stringify(this.state.center))
+  }
 
+  render() {
+    return (
+      <div>
+        <YMaps>
+          <Map
+            width="100%"
+            height="600px"
+            state={mapState}
+            instanceRef={map => this.setState({ map })}
+            onBoundsChange={this.onBoundsChange}
+          />
+
+        </YMaps>
+        <div>Center: {JSON.stringify(this.state.center)}</div>
+        <button onClick={() => this.instPoint()}>Ok</button>
+      </div>
+    );
+  }
+}
 export default Ymap
