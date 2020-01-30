744f348f29d7b8e084753bd3623122fd133f7eb6
zaa <zaa@tk-barn.ru>
2020-01-30 10:04:49 +0700

    Добавил заголовок в тесты для линта

--- src/components/Myfullname.test.js
+++ src/components/Myfullname.test.js
@@ -1,8 +1,9 @@
+/* eslint-env jest */
 
-import Myfullname from "./Myfullname"
+import Myfullname from './Myfullname'
 
-const footer = Myfullname({'firstName': 'Алексей', 'lastName': 'Зубенко'})
-//передаем функциональнльному компоненту данные и смотрим в снимке что получаем
-it("Тест футера", () => {
-    expect(footer).toMatchSnapshot()
-});
+const footer = Myfullname({ firstName: 'Алексей', lastName: 'Зубенко' })
+
+it('Тест футера', () => {
+  expect(footer).toMatchSnapshot()
+})
--- src/components/pointapp/Pointapp.test.js
+++ src/components/pointapp/Pointapp.test.js
@@ -1,6 +1,6 @@
+/* eslint-env jest */
+
 import Pointapp from './Pointapp'
 
 const point = new Pointapp()
-it('Тест рендер компонента содержащего список точек', () => {
-  expect(point).toMatchSnapshot()
-})
+it('Тест рендер компонента содержащего список точек', () => { expect(point).toMatchSnapshot() })
