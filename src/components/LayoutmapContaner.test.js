f3ebffb22238d43890b90d1dc0362435de494ef1
zaa <zaa@tk-barn.ru>
2020-02-04 13:17:19 +0700

    Убрал тестовые тесты

--- src/components/LayoutmapContaner.test.js
+++ src/components/LayoutmapContaner.test.js
@@ -4,23 +4,7 @@ import LayoutmapContaner from './LayoutmapContaner'
 
 /* eslint-env jest */
 
-/** пока не рендерим весь большой компонент */
-/*
 test('тест функции компонента  ', () => {
   const { asFragment } = render(<LayoutmapContaner />)
   expect(asFragment()).toMatchSnapshot()
-})
-*/
-const var1 = new LayoutmapContaner()
-
-test('тестим а свойство', () => {
-  expect(var1.a()).toBe(3)
-})
-
-test('тестим в свойство', () => {
-  expect(var1.b(2, 3)).toBe(6)
-})
-
-it('взаимодействие функций', () => {
-  expect(var1.b(2, var1.a())).toBe(6)
 })
